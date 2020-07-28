0c567ac7d81eb31fd9a48974a6a03d0151e56509
mrasahi <makotorasahi@gmail.com>
2020-07-27 17:57:51 -0700

    answer index randomizer working

--- assets/js/codequiz.js
+++ assets/js/codequiz.js
@@ -1,48 +1,32 @@
 // Question Qurerry
-
 let q0 = {
     question: 'What is my name?',
-    choice1: 'Quintin',
-    choice2: 'Calum',
-    choice3: 'Makoto',
-    choice4: 'Big Mike',
+    choices: ['Quintin', 'Calum', 'Makoto', 'Big Mike'],
     answer: 'Makoto'
 }
 
 let q1 = {
     question: 'What is my last name?',
-    choice1: 'Asahi',
-    choice2: 'Quintin',
-    choice3: 'Calum',
-    choice4: 'Another Mike',
-    answer: 'Asahi',
+    choices: ['Asahi', 'Quintin', 'Calum', 'Another Mike'],
+    answer: 'Asahi'
 }
 
 let q2 = {
     question: 'What is 2 + 2',
-    choice1: '4',
-    choice2: '2',
-    choice3: '3',
-    choice4: '6',
-    answer: '4',
+    choices: ['4', '2', '3', '6',],
+    answer: '4'
 }
 
 let q3 = {
     question: 'THE ANSWER IS 2',
-    choice1: 'WRONG 1',
-    choice2: 'YEES',
-    choice3: 'NOO',
-    choice4: 'NOT THIS ONE',
-    answer: 'YEES',
+    choices: ['WRONG 1', 'YEES', 'NOO', 'NOT THIS ONE'],
+    answer: 'YEES'
 }
 
 // let q = {
 //     question: '',
-//     choice1: '',
-//     choice2: '',
-//     choice3: '',
-//     choice4: '',
-//     answer: '',
+//     choices: [],
+//     answer: ''
 // }
 
 
@@ -51,13 +35,27 @@ let qlist = [q0, q1, q2, q3]
 let score = 0
 let time = 90
 let randomquestion
+let randomanswerlist = []
 
 // Question Querry randomizer
 function qrandom() {
     let i = [Math.floor(Math.random() * qlist.length)]
     randomquestion = qlist[i]
     qlist.splice(i, 1)
+    randomanswers()
     console.log(qlist)
+}
+
+// Randomize answer index function
+function randomanswers() {
+    for (let i = randomquestion.choices.length; i > 0; i--) {
+        let r = randomquestion.choices[Math.floor(Math.random() * randomquestion.choices.length)]
+        randomanswerlist.push(r)
+        randomquestion.answer = randomquestion.choices.splice(r, '')
+    }
+    randomquestion.choices = randomanswerlist
+    console.log(randomquestion)
+    randomanswerlist = []
 }
 
 // I need to add datasets to each answer
@@ -67,13 +65,13 @@ function addq() {
     if (qlist.length === 0) {
         console.log('no u')
         return
+        // endgame functin goes here
     }
     qrandom()
     document.getElementById('q-prompt').textContent = randomquestion.question
-    document.getElementById('answer-1').textContent = randomquestion.choice1
-    document.getElementById('answer-2').textContent = randomquestion.choice2
-    document.getElementById('answer-3').textContent = randomquestion.choice3
-    document.getElementById('answer-4').textContent = randomquestion.choice4
+    for (let i = 0; i < randomquestion.choices.length; i++) {
+        document.getElementById(`answer-${i}`).textContent = randomquestion.choices[i]
+    }
 }
 
 
@@ -86,6 +84,7 @@ function answerCheck() {
         console.log('incorrect')
     }
 }
+
 
 // This could be cleaned up better
 // Answer 1 - 4 clicked
